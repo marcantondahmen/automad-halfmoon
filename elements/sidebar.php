d089154b03436068c45cf674b5f0d009aee17104
Marc Anton Dahmen <mail@marcdahmen.de>
2021-01-03 22:40:39 +0100

    Update sidebar.php

--- elements/sidebar.php
+++ elements/sidebar.php
@@ -1,45 +1,52 @@
-<@ snippet link @>
+<@~ snippet link ~@>
 	<a 
 	href="@{ url }" 
-	class="sidebar-link<@ if @{ :currentPath } @><@ if @{ :current } or @{ :level } = 1 @> active<@ end @><@ end @>">
-		@{ title } @{ :icon }
+	class="sidebar-link d-flex justify-content-between<@ if @{ :current } @> active<@ end @>">
+		@{ title }
+		<@ newPagelist { type: 'children' } @>
+		<@ if @{ :pagelistCount } @>
+			<span>
+				<@ Automad/Bootstrap/Icon {
+					icon: 'chevron-right',
+					w: '0.9em',
+					h: '0.9em',
+					viewBox: '0 -2 16 16'
+				} @>
+			</span>
+		<@ end @>
 	</a>
-<@ end @>
+<@~ end ~@>
 <div class="sidebar py-5">
 	<div class="sidebar-menu">
-		<@ newPagelist {
-			type: 'children',
-			context: '/'
-		} @>
-		<@ foreach in pagelist @>
-			<@ link @>
-		<@ end @>
-		<@ newPagelist {
+		<# Breadcrumbs #>
+		<@~ newPagelist {
 			type: 'breadcrumbs'
-		} @>
-		<@ if @{ :pagelistCount } > 2 and @{ :level } @>
+		} ~@>
+		<@~ if @{ :level } > 1 @>
+			<@~ foreach in pagelist @>
+				<@~ if not @{ :current } and @{ :level } @>
+					<@~ link @>
+				<@~ end @>
+			<@~ end ~@>
 			<div class="sidebar-divider my-15"></div>
-			<@ foreach in pagelist @>
-				<@ if @{ :level } > 1 @>
-					<@ newPagelist { type: 'children' } @>
-					<@ if @{ :pagelistCount } @>
-						<@ set { :icon: '/' } @>
-					<@ else @>
-						<@ set { :icon: ' ' } @>
-					<@ end @>
-					<@ link @>
-				<@ end @>
-			<@ end @>
-		<@ end @>
-		<@ set { :icon: ' ' } @>
-		<@ newPagelist {
+		<@~ end @>
+		<# Siblings #>
+		<@~ newPagelist {
+			type: 'siblings',
+			excludeCurrent: false
+		} @>
+		<@~ foreach in pagelist @>
+			<@~ link @>
+		<@~ end @>
+		<# Children #>
+		<@~ newPagelist {
 			type: 'children'
 		} @>
-		<@ if @{ :pagelistCount } and @{ :level } @>
+		<@~ if @{ :pagelistCount } @>
 			<div class="sidebar-divider my-15"></div>
-			<@ foreach in pagelist @>
-				<@ link @>
-			<@ end @>
-		<@ end @>
+			<@~ foreach in pagelist @>
+				<@~ link @>
+			<@~ end @>
+		<@~ end @>
 	</div>
 </div>
