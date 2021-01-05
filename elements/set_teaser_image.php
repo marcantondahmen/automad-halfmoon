edaeba4ccafa68d882fa0cc4728575e6c063cd79
Marc Anton Dahmen <mail@marcdahmen.de>
2021-01-05 23:07:08 +0100

    Update set_teaser_image.php

--- elements/set_teaser_image.php
+++ elements/set_teaser_image.php
@@ -1,9 +1,9 @@
 <# Reset variable to false in case there is no match. #>
 <@ set { :imageTeaser: false } @>
 <# Try to get image from variable. #>
-<@ with @{ imageTeaser | def ('*.jpg, *.png, *.gif') } { width: 800 } ~@>
+<@ with @{ imageTeaser } { width: 800 } ~@>
 	<@ set { :imageTeaser: @{ :fileResized } } @>
 <@~ else ~@>
-	<# Else try to get first image from content. #>
+	<# Else try a possible working external link or get first image from content. #>
 	<@ set { :imageTeaser: @{ imageTeaser | def (@{ +main | findFirstImage }) } } @>
 <@~ end ~@>
