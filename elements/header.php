d2f0b6b61acfd6a447fa1ba7bf2f25e1aec54c00
Marc Anton Dahmen <mail@marcdahmen.de>
2025-05-24 11:45:32 +0200

    feat: use new meta tags engine and syntax highlighting of Automad version 2

--- elements/header.php
+++ elements/header.php
@@ -7,30 +7,32 @@
 	@{ imageTeaser }
 	@{ notificationNoSearchResults }
 	#>
-	<@ set {
-		:tagTitle: @{ metaTitle | def("@{ sitename } / @{ title | def('404') }") },
-		:tagDescription: @{ metaDescription | def(@{ +main }) | stripTags }
-	} @>
-	<title>@{ :tagTitle }</title>
-	<@ Automad/MetaTags {
-		description: @{ :tagDescription },
-		ogTitle: @{ :tagTitle },
-		ogDescription: @{ :tagDescription },
-		ogImage: @{ imageOpenGraph },
-		twitterCard: 'summary_large_image'
-	} @>
+	<?php if (substr(AM_VERSION, 0, 1) == '1') { ?>
+		<@ set {
+			:tagTitle: @{ metaTitle | def("@{ sitename } / @{ title | def('404') }") },
+			:tagDescription: @{ metaDescription | def(@{ +main }) | stripTags }
+		} @>
+		<title>@{ :tagTitle }</title>
+		<@ Automad/MetaTags {
+			description: @{ :tagDescription },
+			ogTitle: @{ :tagTitle },
+			ogDescription: @{ :tagDescription },
+			ogImage: @{ imageOpenGraph },
+			twitterCard: 'summary_large_image'
+		} @>
+		<link href="/packages/@{ theme }/css/highlightjs.css" rel="stylesheet" />
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/highlight.min.js"></script>
+		<script>hljs.initHighlightingOnLoad();</script>
+	<?php } ?>
 	<@ favicons.php @>
 	<link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/css/halfmoon-variables.min.css" rel="stylesheet" />
 	<link href="/packages/@{ theme }/css/custom.css" rel="stylesheet" />
-	<link href="/packages/@{ theme }/css/highlightjs.css" rel="stylesheet" />
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/highlight.min.js"></script>
-	<script>hljs.initHighlightingOnLoad();</script>
 	
 	@{ customProperties | replace ('/^(.+)$/', '<style>:root{$1}</style>') }
 	@{ itemsHeader }
 
-	<?php if (!empty($_COOKIE['halfmoon_preferredMode']) && $_COOKIE['halfmoon_preferredMode'] == 'dark-mode') { 
-		echo '<@ set { :class: "dark-mode" } @>';
+	<?php if (!empty($_COOKIE['halfmoon_preferredMode']) && $_COOKIE['halfmoon_preferredMode'] == 'dark-mode') {
+	    echo '<@ set { :class: "dark-mode" } @>';
 	} ?>
 </head>
 <body class="with-custom-webkit-scrollbars with-custom-css-scrollbars @{ :class }" data-set-preferred-mode-onload="true">
