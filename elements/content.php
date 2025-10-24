3b0e171dd5b5f8635d73c0616967e29b86ca4c48
Marc Anton Dahmen <mail@marcdahmen.de>
2025-10-24 21:07:17 +0200

    feat: add compatibility with the latest alpha release

--- elements/content.php
+++ elements/content.php
@@ -12,7 +12,5 @@
 	replace('/<table>/', '<table class="table table-no-outer-padding">') |
 	replace('/class="am-button"/', 'class="btn"') |
 	replace('/textarea class="am-input"/', 'textarea class="form-control mt-5 mb-10"') |
-	replace('/class="am-input"/', 'class="form-control my-5"') |
-	replace('/<blockquote>/', '<blockquote class="m-0 pl-20 border-left font-italic">') |
-	replace('/<figcaption([^>]*)>/s', '<figcaption$1 class="text-muted m-0 pl-20 pt-10 border-left">&mdash; ')
+	replace('/class="am-input"/', 'class="form-control my-5"') 
 }
